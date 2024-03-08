4ffcc558dc2e17751717438db6d3ff1432ad38fd
河内真輝 <>
2024-03-09 00:54:33 +0900

    手順８

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,6 +8,8 @@ public class Ball : MonoBehaviour
     // ↓これを記載する
     public float moveSpeed;
 
+    public AudioClip coinGet;
+
     private Rigidbody rb;
 
     void Start()
@@ -22,5 +24,17 @@ public class Ball : MonoBehaviour
         float moveV = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveH, 0, moveV);
         rb.AddForce(movement * moveSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // もしもぶつかった相手に「Coin」という「Tag」がついていたならば（条件）
+        if (other.CompareTag("Coin"))
+        {
+            // ぶつかった相手を破壊する（実行）
+            Destroy(other.gameObject);
+
+            AudioSource.PlayClipAtPoint(coinGet, transform.position);
+        }
     }
 }
