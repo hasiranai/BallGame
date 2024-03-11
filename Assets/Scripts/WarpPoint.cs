7ab664d8fe6b9b6bf6e5661cebcd8a9322b24219
河内真輝 <>
2024-03-11 16:28:09 +0900

    手順１１

---
+++ Assets/Scripts/WarpPoint.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpPoint : MonoBehaviour
+{
+    public Vector3 pos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // （考え方）触れた瞬間にボールに新しい位置情報をセットする
+        // 「0.5f」のように「小数」を使用する場合には必ず「f」を書くこと（ポイント）
+        // 「f」は「float（浮動小数点）」の略
+        other.gameObject.transform.position = pos;
+    }
+}
